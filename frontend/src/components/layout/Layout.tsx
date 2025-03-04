0eed1b97dc6b8cb7a9ea7a83f46d0a704b15e52a
Mk016 <mahendrakumawat80224@gmail.com>
2025-03-04 23:33:02 +0530

    update layout.tsx

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ import BottomNav from './BottomNav'
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+    
       <main className="flex-grow container mx-auto px-4 py-8">
         <Outlet />
       </main>
