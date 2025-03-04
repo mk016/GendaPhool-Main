75cee8b39f90ca5577c7c16a400a73b0498a77a9
Mk016 <mahendrakumawat80224@gmail.com>
2025-03-04 23:36:06 +0530

    add some routes in Router.tsx

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import GPDaily from '../pages/Home';
-import GPStore from '../store/GP_store/Home';
-import About from '../pages/About';
+import GPStore from '../pages/store/GP_store/Home';
 import Signup from '../components/forms/signup/Signup';
 import Login from '../components/forms/login/Login';
 import { StoreProvider } from '../context/StoreContext';
@@ -15,7 +14,13 @@ import ChooseGarlands from '../components/forms/signup/AccountSetup/ChooseGarlan
 import YourPackage from '../components/forms/signup/AccountSetup/YourPackage';
 import BestFit from '../components/forms/signup/AccountSetup/BestFit';
 import SetupLocation from '../components/forms/signup/AccountSetup/SetupLocation';
-
+import SearchPage from '../pages/store/GP_store/pages/SearchPage';
+import Wallet from '../features/Wallet/wallet';
+import PaymentOptions from '../features/Wallet/PaymentOptions';
+import ManageMySubscription from '../features/Wallet/Subscription/ManageMySubscription';
+import MorePage from '../pages/More/home';
+import Refer from '../pages/Refer/Refer';
+import Profile from '../pages/More/Account&Perferenecs/Profile/Profile';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -32,10 +37,6 @@ const router = createBrowserRouter([
       {
         path: '/store',
         element: <GPStore />,
-      },
-      {
-        path: '/about',
-        element: <About />,
       },
       {
         path: '/signup',
@@ -80,6 +81,34 @@ const router = createBrowserRouter([
       {
         path: '/setup-location',
         element: <SetupLocation />
+      },
+      {
+        path: '/store/search',
+        element: <SearchPage />
+      },
+      {
+        path: '/wallet',
+        element: <Wallet />
+      },
+      {
+        path: '/payment-options',
+        element: <PaymentOptions />
+      },
+      {
+        path: '/manage-my-subscription',
+        element: <ManageMySubscription />
+      },
+      {
+        path: '/more',
+        element: <MorePage />
+      },
+      {
+        path: '/refer',
+        element: <Refer />
+      },
+      {
+        path: '/account',
+        element: <Profile />
       }
     ],
   },
